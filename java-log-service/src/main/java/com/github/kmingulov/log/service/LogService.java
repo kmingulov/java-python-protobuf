5a3b6d2f199aa82319826b6c1f8b85d7c2a51ebd
Kirill T. Mingulov <k.t.mingulov@gmail.com>
2019-08-01 19:26:08 +0200

    Remove unused dependencies

--- java-log-service/src/main/java/com/github/kmingulov/log/service/LogService.java
+++ java-log-service/src/main/java/com/github/kmingulov/log/service/LogService.java
@@ -3,7 +3,6 @@ package com.github.kmingulov.log.service;
 import com.github.kmingulov.log.LogServiceProto.LogMessage;
 import com.github.kmingulov.log.LogServiceProto.LogMessageList;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface LogService {
